45faed06cfe746829b5ddd5771c41a8ca8c742f0
Dmitry Novik <dmitry.novik@gmail.com>
2018-12-11 15:41:04 +1100

    adding NullSafe

--- Null.UnitTests/NullSafeTest.cs
+++ Null.UnitTests/NullSafeTest.cs
@@ -1,69 +1,82 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Execution;
 using Xunit;
 
 namespace Null.UnitTests
 {
-    public class DirtyObject : NullObject
+    public class NullSafeTest
     {
-        public string Name { get; set; }
-    }
+        public class TestObject
+        {
+            public string Name { get; set; }
+        }
 
-    public class NullObjectTest
-    {
         [Fact]
         public void Pristine_Equals_Null_Should_Be_True()
         {
-            var pristine = new DirtyObject();
-            (pristine == null).Should().Be(true);
+            var pristine = new NullSafe<TestObject>();
+            using (new AssertionScope())
+            {
+                (pristine == null).Should().Be(true);
+                TestObject to = pristine;
+                to.Name.Should().BeNull();
+            }
         }
 
         [Fact]
         public void Dirty_Equals_Null_Should_Be_False()
         {
-            var dirty = new DirtyObject { Name = "some" };
-            (dirty == null).Should().Be(false);
+            var dirty = new NullSafe<TestObject>(new TestObject { Name = "some" });
+            using (new AssertionScope())
+            {
+                (dirty == null).Should().Be(false);
+                TestObject to = dirty;
+                to.Name.Should().Be("some");
+            }
         }
 
         [Fact]
         public void Pristine_Equals_Self_Should_Be_True()
         {
-            var pristine = new DirtyObject();
+            var pristine = new NullSafe<TestObject>();
             (pristine == pristine).Should().Be(true);
         }
 
         [Fact]
         public void Dirty_Equals_Self_Should_Be_True()
         {
-            var dirty = new DirtyObject { Name = "some" };
+            var dirty = new NullSafe<TestObject>(new TestObject() { Name = "some" });
             (dirty == dirty).Should().Be(true);
         }
 
         [Fact]
         public void Pristine_Not_Equals_Null_Should_Be_False()
         {
-            var pristine = new DirtyObject();
+            var pristine = new NullSafe<TestObject>();
             (pristine != null).Should().Be(false);
         }
 
         [Fact]
         public void Dirty_Not_Equals_Null_Should_Be_True()
         {
-            var dirty = new DirtyObject { Name = "some" };
+            var dirty = new NullSafe<TestObject>(new TestObject() { Name = "some" });
             (dirty != null).Should().Be(true);
         }
 
         [Fact]
         public void Pristine_Not_Equals_Self_Should_Be_False()
         {
-            var pristine = new DirtyObject();
+            var pristine = new NullSafe<TestObject>();
             (pristine != pristine).Should().Be(false);
         }
 
         [Fact]
         public void Dirty_Not_Equals_Self_Should_Be_False()
         {
-            var dirty = new DirtyObject { Name = "some" };
+            var dirty = new NullSafe<TestObject>(new TestObject() { Name = "some" });
             (dirty != dirty).Should().Be(false);
         }
+
     }
+
 }
---
+++ Null/NullSafe.cs
@@ -0,0 +1,48 @@
+﻿namespace Null
+{
+    public class NullSafe<T> where T : class, new()
+    {
+        private T _value;
+
+        public NullSafe()
+        {
+            _value = new T();
+            IsNull = true;
+        }
+
+        public NullSafe(T value)
+        {
+            _value = value;
+            IsNull = false;
+        }
+
+        public bool IsNull { get; private set; }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                IsNull = value == null;
+                _value = value ?? new T();
+            }
+        }
+
+        public static implicit operator T(NullSafe<T> arg) => arg.Value;
+
+        public static implicit operator NullSafe<T>(T arg) => new NullSafe<T>(arg);
+
+        public static bool operator ==(NullSafe<T> a, NullSafe<T> b)
+        {
+            if (ReferenceEquals(a, null))
+                return ReferenceEquals(b, null) || b.IsNull;
+            if (ReferenceEquals(b, null))
+                return a.IsNull;
+
+            return ReferenceEquals(a, b);
+        }
+
+        public static bool operator !=(NullSafe<T> a, NullSafe<T> b) => !(a == b);
+    }
+
+}
